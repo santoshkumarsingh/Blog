2a51c79a7706a772da7dd5341c04d864ed7ebbe6
Franco T. Robles <robles.franco@gmail.com>
2014-08-18 21:53:19 +0800

    HTML posts and ASP.NET MVC
    Allow HTML tags when posting with ASP.NET MVC

---
+++ HtmlInputsInMvc/HtmlInputsInMvc/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlInputsInMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ValidateInput(false)]
+        public ActionResult AddComment(string comment)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(comment, "<script>.*?</script>")) {
+                throw new HttpException(500, "Potantially malicious Request.QueryString detected.");
+            }
+            else {
+                ViewBag.Message = comment;
+            }
+            return View("Index");
+        }
+	}
+}
